5f1852c1f2a255dc162b84d8d4d47fac746756e3
root <dkxzq@163.com>
2015-01-25 16:13:56 +0800

    first

---
+++ src/Data/OrderDate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ATable.Data
+{
+    public class OrderDetail
+    {
+        public int OrderDetaild { set; get; }
+        public int OrderId { set; get; }
+         
+    }
+}
---
+++ src/Data/getData.ashx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ATable.Data
+{
+    /// <summary>
+    /// getData 的摘要说明
+    /// </summary>
+    public class getData : IHttpHandler
+    {
+        protected static List<OrderDetail> details = new List<OrderDetail>();
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+
+            context.Response.Write("Hello World");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
